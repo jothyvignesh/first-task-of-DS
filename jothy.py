aa733b1b36127d053e2852a9f9d6115dd9472341
jothyvignesh <122095161+jothyvignesh@users.noreply.github.com>
2023-01-06 17:39:32 +0530

    first completed task of ds
    ds task completed

---
+++ jothy.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    if((a%2==0) and (b%2==0)):
+        print("these are even numbers")
+        c=a+b
+        print("The value is ",c)
+    else:
+        print('The value given are odd number')
+        c=a*b
+        print("The multiplication of two numbers:",c)
